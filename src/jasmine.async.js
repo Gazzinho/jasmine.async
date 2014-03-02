9f5fac3aca7b5b05dc3af2e8a1f02cbf54868e0b
Gary Collins <gary.p.collins@gmail.com>
2014-03-02 20:35:20 +0000

    Added support for configurable timeout and timeout message
    Added optional waitsForMesage and timeout arguments to the
    async.beforeEach, async.afterEach and async.it methods.
    Also added lint globals header and includes the xit proxy.

--- src/jasmine.async.js
+++ src/jasmine.async.js
@@ -1,20 +1,29 @@
+/*globals runs, waitsFor, xit */
 this.AsyncSpec = (function(global){
+    'use strict';
 
-  // Private Methods
-  // ---------------
-  
-  function runAsync(block){
+    // Private Methods
+    // ---------------
+
+  function runAsync(block, waitsForMessage, timeout){
     return function(){
       var done = false;
       var complete = function(){ done = true; };
+      waitsForMessage = waitsForMessage || 'jasmine.async block to run';
+      timeout = timeout || 5000;
 
       runs(function(){
-        block(complete);
+        try{
+          block(complete);
+        }catch(error){
+          complete();
+          throw error;
+        }
       });
 
       waitsFor(function(){
         return done;
-      });
+      }, waitsForMessage, timeout);
     };
   }
 
@@ -28,19 +37,19 @@ this.AsyncSpec = (function(global){
   // Public API
   // ----------
 
-  AsyncSpec.prototype.beforeEach = function(block){
-    this.spec.beforeEach(runAsync(block));
+  AsyncSpec.prototype.beforeEach = function(block, waitsForMessage, timeout){
+    this.spec.beforeEach(runAsync(block, waitsForMessage, timeout));
   };
 
-  AsyncSpec.prototype.afterEach = function(block){
-    this.spec.afterEach(runAsync(block));
+  AsyncSpec.prototype.afterEach = function(block, waitsForMessage, timeout){
+    this.spec.afterEach(runAsync(block, waitsForMessage, timeout));
   };
 
-  AsyncSpec.prototype.it = function(description, block){
+  AsyncSpec.prototype.it = function(description, block, waitsForMessage, timeout){
     // For some reason, `it` is not attached to the current
     // test suite, so it has to be called from the global
     // context.
-    global.it(description, runAsync(block));
+    global.it(description, runAsync(block, waitsForMessage, timeout));
   };
 
   AsyncSpec.prototype.xit = xit;
